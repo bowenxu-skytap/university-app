dab525472eef5cb9c0ea3e276b2f3f4dd0a5d300
Bowen Xu <jimmyxbw@gmail.com>
2019-01-24 23:33:11 -0500

    add edit student feature

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,10 @@ class StudentsController < ApplicationController
     @student = Student.new
   end
 
+  def edit
+    @student = Student.find(params[:id])
+  end
+
   def create
     @student = Student.new(student_params)
     if @student.save
@@ -20,6 +24,17 @@ class StudentsController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(student_params)
+      flash[:notice] = "You have successfully updated your profile"
+      redirect_to @student
+    else
+      render 'edit'
+    end
+
   end
 
   private
